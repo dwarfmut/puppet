4d8ab859c1ea8af293d973480ec292f67f2a8ed1
Daniel Faria <daniel.faria@abril.com.br>
2015-03-31 15:44:06 -0300

    teste

--- modules/seya/manifests/vclconfig.pp
+++ modules/seya/manifests/vclconfig.pp
@@ -3,7 +3,6 @@ class seya::vclconfig {
   include varnish
 
   varnish::backend { 'seya': host => '192.168.0.1', port => '80', vhost => 'seya.dwarfmut.com.br' }
-  varnish::backend { 'pegasus': host => '192.168.0.2', port => '80', vhost => 'pegasus.dwarfmut.com.br' }
   varnish::backend { 'andromeda': host => '192.168.0.3', port => '80', vhost => 'andromeda.dwarfmut.com.br' }
 
 }
