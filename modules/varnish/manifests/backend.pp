411f58205e79a39caeb9db597dde17d1b2f08945
Daniel Faria <daniel.faria@abril.com.br>
2015-03-31 11:21:18 -0300

    [seya] add varnish module varnish

---
+++ modules/varnish/manifests/backend.pp
@@ -0,0 +1,18 @@
+# Backend.pp
+
+define varnish::backend (
+  $host,
+  $port,
+  $vhost,
+  $connect_timeout = "90s",
+  $first_byte_timeout = "90s",
+  $between_bytes_timeout = "90s",
+) {
+
+  concat::fragment {"${title}-backend":
+    target  => "${varnish::vcl::include}/backends.vcl",
+    content => template('varnish/includes/backends.erb'),
+    order   =>  '02',
+  }
+
+}
