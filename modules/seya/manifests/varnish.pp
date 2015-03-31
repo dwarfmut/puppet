7f124230e0337c699ffb1b39f5622059c2874706
Daniel Faria <daniel.faria@abril.com.br>
2015-03-31 14:15:51 -0300

    testee

--- modules/seya/manifests/varnish.pp
+++ modules/seya/manifests/varnish.pp
@@ -1,7 +1,7 @@
 class seya::varnish {
 
   varnish::backend { 'seya': host => '192.168.0.1', port => '80', vhost => 'seya.dwarfmut.com.br' }
-  varnish::backend { 'pegasus': host => '192.168.0.1', port => '80', vhost => 'seya.dwarfmut.com.br' }
-  varnish::backend { 'andromeda': host => '192.168.0.1', port => '80', vhost => 'seya.dwarfmut.com.br' }
+  varnish::backend { 'pegasus': host => '192.168.0.2', port => '80', vhost => 'pegasus.dwarfmut.com.br' }
+  varnish::backend { 'andromeda': host => '192.168.0.3', port => '80', vhost => 'andromeda.dwarfmut.com.br' }
 
 }
